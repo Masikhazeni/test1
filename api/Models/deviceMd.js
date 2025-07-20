af5d01effed6f9a4ccf8eb8cd180731060573514
Masikhazeni <masikhazeni210@gmail.com>
2025-07-20 16:51:30 +0330

    add device model
    add and complete deviceMd

---
+++ api/Models/deviceMd.js
@@ -0,0 +1,29 @@
+import pool from "../db";
+
+class device{
+    static async creat({userId,deviceName}){
+        const[result]=await pool.execute(
+            'INSERT INTO devices (userId,deviceName) VALUES (?,?)',[userId,deviceName]
+        )
+        return result.insertId
+    }
+    static async findById(id){
+        const[rowes]=await pool.execute(
+            'SELECT * FROM devices WHERE id=?',[id]
+        )
+        return rowes[0] ||null
+    }
+    static async findByUserId(userId){
+        const[rowes]=await pool.execute(
+            'SELECT * FROM devices WHERE userId=?',[userId]
+        )
+        return rowes[0] ||null
+    }
+    static async delet(id){
+       await pool.execute(
+            'DELET FROM devices WHERE id=?',[id]
+        )
+    }
+}
+
+export default device
